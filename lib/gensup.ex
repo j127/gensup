da7ec052067612554d217f3372cc82d0a06e9832
J. Cohen <hostelmarketing@gmail.com>
2020-04-08 15:49:54 -0700

    Initial commit

---
+++ lib/gensup.ex
@@ -0,0 +1,18 @@
+defmodule Gensup do
+  @moduledoc """
+  Documentation for `Gensup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gensup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gensup_test.exs
@@ -0,0 +1,8 @@
+defmodule GensupTest do
+  use ExUnit.Case
+  doctest Gensup
+
+  test "greets the world" do
+    assert Gensup.hello() == :world
+  end
+end
