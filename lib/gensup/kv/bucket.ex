1956933d626e1392fe84c1c3666cb02880c6e4a7
J. Cohen <hostelmarketing@gmail.com>
2020-04-08 18:32:40 -0700

    Add note about client/server work with Agents.

--- lib/gensup/kv/bucket.ex
+++ lib/gensup/kv/bucket.ex
@@ -26,6 +26,22 @@ defmodule Gensup.KV.Bucket do
   Delete key from bucket.
 
   Returns the current value of key if it exists.
+
+  A note from the docs:
+
+  The second argument happens on the Agent server. Everything else
+  happens on the client. If you perform expensive computations on the
+  server, it can block other clients from using that server. Example:
+
+  ```
+  def delete(bucket, key) do
+    Process.sleep(1000) # puts client to sleep
+    Agent.get_and_update(bucket, fn dict ->
+      Process.sleep(1000) # puts server to sleep
+      Map.pop(dict, key)
+    end)
+  end
+  ```
   """
   def delete(bucket, key) do
     Agent.get_and_update(bucket, &Map.pop(&1, key))
